0247cf3213f72f1df497fefbde625b4a24a69c4c
Ziyad Zulfikar <56788667+ziyadzulfikar@users.noreply.github.com>
2020-09-04 15:38:21 +0530

    Updates

---
+++ precache-manifest.926371fd8beb367ad47252dc155c1764.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43860817d7746c104ba6b60437afc991",
+    "url": "/React-github/index.html"
+  },
+  {
+    "revision": "9a4191a21d03f3bb4374",
+    "url": "/React-github/static/css/main.9d8e92f8.chunk.css"
+  },
+  {
+    "revision": "0e7da6bdf7fe0d7f0a18",
+    "url": "/React-github/static/js/2.2c37138d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-github/static/js/2.2c37138d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4191a21d03f3bb4374",
+    "url": "/React-github/static/js/main.a5564737.chunk.js"
+  },
+  {
+    "revision": "3fe79cbf172dbb39a5f8",
+    "url": "/React-github/static/js/runtime-main.44b7619c.js"
+  },
+  {
+    "revision": "50899514d78fad6d44f29ac06d8a9dbd",
+    "url": "/React-github/static/media/todo.50899514.jpg"
+  }
+]);
